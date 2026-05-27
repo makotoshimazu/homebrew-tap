2cdaef06eabd284f09ea155a050d942214416c7d
Makoto Shimazu <makoto.shimaz@gmail.com>
2026-05-27 14:17:59 +0900

    Update git-ignore to 0.1.1

--- Formula/git-ignore.rb
+++ Formula/git-ignore.rb
@@ -1,26 +1,26 @@
 class GitIgnore < Formula
   desc "Append templates from github/gitignore to a local .gitignore file"
   homepage "https://github.com/makotoshimazu/git-ignore"
-  version "0.1.0"
+  version "0.1.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.0/git-ignore_0.1.0_darwin_arm64.tar.gz"
-      sha256 "d4f265e3e15f37383412ee4b40d80057833726baa03f6128a69c1646dd3d65a3"
+      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.1/git-ignore_0.1.1_darwin_arm64.tar.gz"
+      sha256 "4a0513be6eb11172e95a565573bc3d320e197475f3daba8264161495596b8f7d"
     else
-      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.0/git-ignore_0.1.0_darwin_amd64.tar.gz"
-      sha256 "a5f9ba2afad00b5aacea4b8c808a627d1f5dfd596433212a1f4d86aa0a6972d3"
+      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.1/git-ignore_0.1.1_darwin_amd64.tar.gz"
+      sha256 "ff944ace833dceba837ea2d6b54e0b437dc47ed9d29ae170ca42cc23a6d680ec"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.0/git-ignore_0.1.0_linux_arm64.tar.gz"
-      sha256 "e39c54be2c7dab22c3e030d051b21ccaf29d1b95b4fa064ec9af3310ba06005e"
+      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.1/git-ignore_0.1.1_linux_arm64.tar.gz"
+      sha256 "8170513a75779ba508c02b61d5c0fa5ebc54f73f17b9100c1e314af9beefcf18"
     else
-      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.0/git-ignore_0.1.0_linux_amd64.tar.gz"
-      sha256 "2f8c0d8693885705b82870397811eb3df6863ac3b3e6263ce168cc1cdeccc216"
+      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.1/git-ignore_0.1.1_linux_amd64.tar.gz"
+      sha256 "03ae9964294f04f0a2f17259cc1ff748062713edfb04c2b96c54f16fe1d84c07"
     end
   end
 
