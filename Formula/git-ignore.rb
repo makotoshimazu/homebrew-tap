52e76bea562a64ce6e136527ef3c41b019bb5d36
Makoto Shimazu <makoto.shimaz@gmail.com>
2026-05-27 20:05:58 +0900

    Update git-ignore to 0.1.3

--- Formula/git-ignore.rb
+++ Formula/git-ignore.rb
@@ -1,26 +1,26 @@
 class GitIgnore < Formula
   desc "Append templates from github/gitignore to a local .gitignore file"
   homepage "https://github.com/makotoshimazu/git-ignore"
-  version "0.1.2"
+  version "0.1.3"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.2/git-ignore_0.1.2_darwin_arm64.tar.gz"
-      sha256 "00efba5c75118147b54f2bee4a0c8048815c8e0b8c09a6f6087be150c785bca5"
+      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.3/git-ignore_0.1.3_darwin_arm64.tar.gz"
+      sha256 "578c813c16dcab54f091e1483f4b7f4cb6e5f0480b2cb8c25335ca1089ac3ddd"
     else
-      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.2/git-ignore_0.1.2_darwin_amd64.tar.gz"
-      sha256 "3554e60f0c35f3404b7feccd408b0aec8db732ee40e1f7f6930d8f7d7d7e126f"
+      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.3/git-ignore_0.1.3_darwin_amd64.tar.gz"
+      sha256 "6e9b30e05e223c8e7b3d1a76581ff71a14fd858b15391b9167ef6124c5ca4aa8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.2/git-ignore_0.1.2_linux_arm64.tar.gz"
-      sha256 "c431baf5f0336cdea34ef0f946dd0c50a96e99647c556e2dd7871d5bd40c79d5"
+      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.3/git-ignore_0.1.3_linux_arm64.tar.gz"
+      sha256 "58e2bf3f874a42b68ce2104b0784b354aad13a4236c3b867a3e7236ad2cb5539"
     else
-      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.2/git-ignore_0.1.2_linux_amd64.tar.gz"
-      sha256 "386207893b90fe86c0c9135739714a860e7d7b4f19be483d3179980dc3641d71"
+      url "https://github.com/makotoshimazu/git-ignore/releases/download/v0.1.3/git-ignore_0.1.3_linux_amd64.tar.gz"
+      sha256 "536b4e9fe679f0fa265642191239de2fc4a610dcf89c5ffb68278ac121bde84c"
     end
   end
 
